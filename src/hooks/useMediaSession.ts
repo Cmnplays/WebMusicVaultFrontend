79d20e66db8fe192cf6d34851d42ff9fcac9a6dc
Aaditya <chaurasiyarita20@gmail.com>
2025-11-24 09:31:22 +0545

    Update useMediaSession.ts
    added basic code for this file

--- src/hooks/useMediaSession.ts
+++ src/hooks/useMediaSession.ts
@@ -1,11 +1,13 @@
-
+import {useAppSelector} from "../store/hook.ts"
 function useMediaSession(controllers){
+const playingSong=useAppSelector(state=>state.song.playingSong)
 if(!"medisession" in navigator){
     return
 }
-
 navigator.mediaSession.metadata= new MediaMetaData({
    title: playingSong.title
 })
+navigator.mediaSession.setActionHandler("nexttrack",controllers.moveToNextSong)
+navigator.mediaSession.setActionHandler("previoustrack",controllers.moveToPreviousSong)
 }
 export default useMediaSession
