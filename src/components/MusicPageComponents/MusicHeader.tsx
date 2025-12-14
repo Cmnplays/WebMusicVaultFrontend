b7fcb7c9aac3b2fd734674ebc9feca16c836ef35
Aaditya <chaurasiyarita20@gmail.com>
2025-12-14 13:03:51 +0545

    Update MusicHeader.tsxui changes

--- src/components/MusicPageComponents/MusicHeader.tsx
+++ src/components/MusicPageComponents/MusicHeader.tsx
@@ -8,7 +8,7 @@ const MusicHeader = ({
   return (
     <div className="flex justify-between items-center">
       <h2 className="text-3xl font-extrabold text-white tracking-tight leading-tight">
-        Your Music Collection
+        Music Collection
       </h2>
       <button
         onClick={handleSorting}
