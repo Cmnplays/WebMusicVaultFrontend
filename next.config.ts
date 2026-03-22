18805ff44c0e2e57c1b61c823e625c00424d7e86
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2026-03-23 00:37:19 +0545

    fix: added vercel proxy configurations for sending httpOnly cookies in cross site

--- next.config.ts
+++ next.config.ts
@@ -12,16 +12,6 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "example.com" },
     ],
   },
-  // Add this section:
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        // This takes whatever is in your NEXT_PUBLIC_API_URL and maps it to /api/v1
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, 
-      },
-    ];
-  },
 };
 
 export default nextConfig;
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 import store from "@/store/store";
 const apiBase = process.env.NEXT_PUBLIC_API_URL;
 const api = axios.create({
-  baseURL: apiBase,
+  baseURL: process.env.NODE_ENV === "production" ? "/api/v1" : process.env.NEXT_PUBLIC_API_URL,
 });
-
 api.interceptors.request.use((config) => {
   const accessToken = store.getState().auth.accessToken;
   if (accessToken) {
